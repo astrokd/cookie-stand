bffe1a38ea30b1d7b9dcdb7da79488ab8e90bcd1
AstroKD <kdreyerinseattle@gmail.com>
2019-10-08 21:20:57 -0700

    add new instances of object

--- js/app.js
+++ js/app.js
@@ -29,10 +29,13 @@ function Shop(shopLocation, minHourlyCustomers, maxHourlyCustomers, avgCookiesPe
 }
 
 //**** Prototype functions ****/
+
+//Get random number of customer function
 Shop.prototype.custPerHourTotals = function() {
   return getRandomIntArray(this.minHourlyCustomers,this.maxHourlyCustomers,hours.length);
 };
 
+//Get cookies sold per hour
 Shop.prototype.cookPerHourTotals = function() {
   this.customerPerHour = this.custPerHourTotals();
   for (var i = 0;i < this.customerPerHour.length;i++) {
@@ -41,6 +44,7 @@ Shop.prototype.cookPerHourTotals = function() {
   return this.cookiesPerHour;
 };
 
+//Get total cookies sold
 Shop.prototype.totCookPerDay = function() {
   for (var x = 0;x < this.cookiesPerHour.length;x++) {
     this.totalCookiesPerDay += this.cookiesPerHour[x];
@@ -48,6 +52,7 @@ Shop.prototype.totCookPerDay = function() {
   return this.totalCookiesPerDay;
 };
 
+//Render location name, cookies per hour and total cookies
 Shop.prototype.render = function() {
   //call cookie hours and totals
   this.cookPerHourTotals();
@@ -69,18 +74,23 @@ Shop.prototype.render = function() {
 };
 
 //********Shop 1******Phinny********* */
-var Phinny = new Shop('Phinny Ridge', 5, 20, 8.8);
+var Phinny = new Shop('Phinny Ridge', 5, 20, 7.8);
+
+//********Shop 2******GreenWood********* */
+var GreenWood = new Shop('Greenwood', 6, 25, 8.5);
+
+//********Shop 3******Ballard********* */
+var Ballard = new Shop('Ballard', 8, 22, 9.1);
+
+//********Shop 4*******WestSeattle******** */
+var WestSeattle = new Shop('West Seattle', 6, 20, 8.8);
+
+//********Shop 5*******DownTown******** */
+var DownTown = new Shop('Downtown', 10, 35, 6.3);
+
 Phinny.render();
-
-//********Shop 2*************** */
-//GreenWood
-
-//********Shop 3*************** */
-//Ballard
-
-//********Shop 4*************** */
-//WestSeattle
-
-//********Shop 5*************** */
-//DownTown
+GreenWood.render();
+Ballard.render();
+WestSeattle.render();
+DownTown.render();
 
